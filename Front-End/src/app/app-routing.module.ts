237860902326691de04c18c53cabae8db071894c
Kartik Hirijaganer <kartikhirijaganer@gmail.com>
2024-04-17 19:03:11 +0530

    added routing in nav elements

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { IncomeComponent } from "./income/income.component";
+import { ExpenseComponent } from "./expense/expense.component";
+import { LiabilityComponent } from "./liability/liability.component";
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'incomes', component: IncomeComponent },
+  { path: 'expenses', component: ExpenseComponent },
+  { path: 'liabilities', component: LiabilityComponent}
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
