6943cbe14c6ba9e84d2c89c0972124b9de949ba9
Kartik-Hirijaganer <65550498+Kartik-Hirijaganer@users.noreply.github.com>
2024-04-22 22:02:39 +0530

    Revert "Completed Income Page"

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -1,26 +1,10 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent } from "./dashboard/dashboard.component";
-import { IncomeComponent } from "./income/income.component";
-import { ExpenseComponent } from "./expense/expense.component";
-import { LiabilityComponent } from "./liability/liability.component";
-
-const appRoutes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'incomes', component: IncomeComponent },
-  { path: 'expenses', component: ExpenseComponent },
-  { path: 'liabilities', component: LiabilityComponent}
-]
+const routes: Routes = [];
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes)
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-
 export class AppRoutingModule { }
--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -1,30 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AgGridModule } from 'ag-grid-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ExpenseComponent } from './expense/expense.component';
-import { IncomeComponent } from './income/income.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { LiabilityComponent } from './liability/liability.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    DashboardComponent,
-    IncomeComponent,
-    ExpenseComponent,
-    LiabilityComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    AgGridModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
