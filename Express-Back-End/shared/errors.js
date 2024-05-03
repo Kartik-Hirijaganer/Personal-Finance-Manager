6885e695e2ce3340aee13da4b15bca7b48df93f2
Kartik Hirijaganer <kartikhirijaganer@gmail.com>
2024-05-03 11:35:02 +0530

    pdf error handling

--- Express-Back-End/shared/errors.js
+++ Express-Back-End/shared/errors.js
@@ -20,7 +20,40 @@ class RecordNotFoundError extends Error {
   }
 }
 
+class GeneratePdfError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "GeneratePdfError",
+    this.errorMessage = message,
+    this.errorCode = 'SYS-0001',
+    this.status = 400
+  }
+}
+
+class HandlebarCompileError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "HandlebarCompileError",
+    this.errorMessage = message,
+    this.errorCode = 'SYS-0002',
+    this.status = 400
+  }
+}
+
+class BrowserLaunchError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "BrowserLaunchError",
+    this.errorMessage = message,
+    this.errorCode = 'SYS-0003',
+    this.status = 400
+  }
+}
+
 module.exports = {
   DatabaseError,
-  RecordNotFoundError
+  RecordNotFoundError,
+  GeneratePdfError,
+  HandlebarCompileError,
+  BrowserLaunchError
 }
