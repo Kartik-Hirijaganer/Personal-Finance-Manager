4c329f7efe008d85f0fb03f571b5ee2668078495
Kartik Hirijaganer <kartikhirijaganer@gmail.com>
2024-05-04 00:20:45 +0530

    updated header

--- Front-End/src/app/header/header.component.ts
+++ Front-End/src/app/header/header.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, UrlSegment } from '@angular/router';
+
+import { UserService } from '../user/user.service';
+import { switchMap } from 'rxjs';
+import { User } from '../user/user.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  public user: string = '';
+  public profile_img: string = '';
 
+  constructor( private userService: UserService, private route: ActivatedRoute ) { }
+
+  ngOnInit(): void {
+    this.route.url.pipe(
+      switchMap((event: UrlSegment[]) => {
+        // const userId = event[0]?.path;
+        const userId = 'username';
+        return this.userService.getUser(userId);
+      })
+    ).subscribe(({user}) => {
+      console.log(user);
+      
+      this.user = user.fname;
+      this.profile_img = user?.profile_img || 'https://www.w3schools.com/howto/img_avatar.png';
+    })
+  }
 }
