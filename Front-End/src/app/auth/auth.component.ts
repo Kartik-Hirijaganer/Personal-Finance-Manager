91ce061976a8d8027cb9f49d8222f7e11b958df9
Kartik Hirijaganer <kartikhirijaganer@gmail.com>
2024-05-05 00:38:51 +0530

    completed login form and routing

--- Front-End/src/app/auth/auth.component.ts
+++ Front-End/src/app/auth/auth.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 import { AuthService } from './auth.service';
 
 @Component({
@@ -6,10 +8,32 @@ import { AuthService } from './auth.service';
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css', '../shared/custom.styles.css']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({});
+  public userId: string = 'username';
+
   constructor( private authService: AuthService ) { }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.loginForm = new FormGroup({
+      email: new FormControl<String>('', [Validators.required, Validators.email]),
+      password: new FormControl<String>('', Validators.required)
+    })
+  }
+
+  onRegister() {
+    
+  }
 
   login() {
     this.authService.isLoginMode = !this.authService.isLoginMode;
+  }
+
+  onSubmit() {
+    console.log(this.loginForm);
   }
 }
